e8faa7072eac77d0e28e063cbf87c33085f43893
gunpreetsiingh <gunpreet@kodinng.com>
2021-08-30 15:40:35 +0530

    Slash screen created. Empty Auth screen created.

---
+++ lib/screens/authentication.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  const Authentication({Key? key}) : super(key: key);
+
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  var size;
+  @override
+  Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Column(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash.dart
@@ -0,0 +1,74 @@
+import 'package:badger/screens/authentication.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  var size;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    navigate();
+  }
+
+  void navigate() async {
+    await Future.delayed(Duration(milliseconds: 2500));
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => Authentication(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image(
+              image: AssetImage('assets/splash-bg.jpg'),
+              fit: BoxFit.cover,
+              height: size.height,
+              width: size.width,
+            ),
+            Container(
+              width: 135,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Badger',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 42,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  LinearProgressIndicator(
+                    color: Colors.black,
+                    backgroundColor: Colors.black.withOpacity(0.30),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
